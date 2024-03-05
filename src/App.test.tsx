1a6aa0f5da7d6dfd4ac8ab30eb61e585518fdee3
Noir Joe <noirjoe@outlook.com>
2024-03-05 14:36:27 +1100

    added reddit fetch and reducers

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,105 +1,16 @@
-import { screen, waitFor } from "@testing-library/react"
-import App from "./App"
-import { renderWithProviders } from "./utils/test-utils"
+// import { screen, waitFor } from "@testing-library/react"
+// import App from "./App"
+// import { renderWithProviders } from "./utils/test-utils"
+import Reddit from "./features/Reddit/Reddit";
 
-test("App should have correct initial render", () => {
-  renderWithProviders(<App />)
+describe('Reddit Class', () => {
+  describe('method: getAccessToken', () => {
+    test('should return a valid access token in type of String', async () => {
+      const userName: string = 'Noir_Joe';
+      const password: string = 'mellow-deflate-umiak-hippo-acuity-barb-drown-camelot-drive-nonunion-camest-diabetes-chapbook-huddle-enforce';
+      const accessToken = await Reddit.getAccessToken(userName, password);
 
-  // The app should be rendered correctly
-  expect(screen.getByText(/learn/i)).toBeInTheDocument()
-
-  // Initial state: count should be 0, incrementValue should be 2
-  expect(screen.getByLabelText("Count")).toHaveTextContent("0")
-  expect(screen.getByLabelText("Set increment amount")).toHaveValue(2)
-})
-
-test("Increment value and Decrement value should work as expected", async () => {
-  const { user } = renderWithProviders(<App />)
-
-  // Click on "+" => Count should be 1
-  await user.click(screen.getByLabelText("Increment value"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("1")
-
-  // Click on "-" => Count should be 0
-  await user.click(screen.getByLabelText("Decrement value"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("0")
-})
-
-test("Add Amount should work as expected", async () => {
-  const { user } = renderWithProviders(<App />)
-
-  // "Add Amount" button is clicked => Count should be 2
-  await user.click(screen.getByText("Add Amount"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("2")
-
-  const incrementValueInput = screen.getByLabelText("Set increment amount")
-  // incrementValue is 2, click on "Add Amount" => Count should be 4
-  await user.clear(incrementValueInput)
-  await user.type(incrementValueInput, "2")
-  await user.click(screen.getByText("Add Amount"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("4")
-
-  // [Negative number] incrementValue is -1, click on "Add Amount" => Count should be 3
-  await user.clear(incrementValueInput)
-  await user.type(incrementValueInput, "-1")
-  await user.click(screen.getByText("Add Amount"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("3")
-})
-
-it("Add Async should work as expected", async () => {
-  const { user } = renderWithProviders(<App />)
-
-  // "Add Async" button is clicked => Count should be 2
-  await user.click(screen.getByText("Add Async"))
-
-  await waitFor(() =>
-    expect(screen.getByLabelText("Count")).toHaveTextContent("2"),
-  )
-
-  const incrementValueInput = screen.getByLabelText("Set increment amount")
-  // incrementValue is 2, click on "Add Async" => Count should be 4
-  await user.clear(incrementValueInput)
-  await user.type(incrementValueInput, "2")
-
-  await user.click(screen.getByText("Add Async"))
-  await waitFor(() =>
-    expect(screen.getByLabelText("Count")).toHaveTextContent("4"),
-  )
-
-  // [Negative number] incrementValue is -1, click on "Add Async" => Count should be 3
-  await user.clear(incrementValueInput)
-  await user.type(incrementValueInput, "-1")
-  await user.click(screen.getByText("Add Async"))
-  await waitFor(() =>
-    expect(screen.getByLabelText("Count")).toHaveTextContent("3"),
-  )
-})
-
-test("Add If Odd should work as expected", async () => {
-  const { user } = renderWithProviders(<App />)
-
-  // "Add If Odd" button is clicked => Count should stay 0
-  await user.click(screen.getByText("Add If Odd"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("0")
-
-  // Click on "+" => Count should be updated to 1
-  await user.click(screen.getByLabelText("Increment value"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("1")
-
-  // "Add If Odd" button is clicked => Count should be updated to 3
-  await user.click(screen.getByText("Add If Odd"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("3")
-
-  const incrementValueInput = screen.getByLabelText("Set increment amount")
-  // incrementValue is 1, click on "Add If Odd" => Count should be updated to 4
-  await user.clear(incrementValueInput)
-  await user.type(incrementValueInput, "1")
-  await user.click(screen.getByText("Add If Odd"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("4")
-
-  // click on "Add If Odd" => Count should stay 4
-  await user.clear(incrementValueInput)
-  await user.type(incrementValueInput, "-1")
-  await user.click(screen.getByText("Add If Odd"))
-  expect(screen.getByLabelText("Count")).toHaveTextContent("4")
-})
+      expect(typeof accessToken).toBe('string');
+    });
+  })
+});
---
+++ src/features/redditPosts/redditPostsSlice.ts
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+interface RedditPostData {
+  author: string;
+  title: string;
+  selftext: string;
+  ups: number;
+  num_comments: number;
+  media_link?: string;
+}
+
+interface RedditPostsState {
+  posts: RedditPostData[];
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
+}
+
+const initialState: RedditPostsState = {
+  posts: [],
+  status: "idle",
+  error: null,
+};
+
+// async thunk action for fetching Reddit posts
+export const fetchRedditPosts = createAsyncThunk(
+  "redditPosts/fetchPosts",
+  async (subReddit: string) => {
+    try {
+      const respond = await fetch(`https://www.reddit.com/r/${subReddit}.json`);
+      const json = await respond.json();
+
+      return json.data.children.map((child: any) => ({
+        author: child.data.author,
+        title: child.data.title,
+        selftext: child.data.selftext,
+        ups: child.data.ups,
+        num_comments: child.data.num_comments,
+      }));
+    } catch (error) {
+      console.error("Error fetching Reddit posts:", error);
+    }
+  },
+);
+
+const redditPostsSlice = createSlice({
+  name: "redditPosts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRedditPosts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchRedditPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.posts = action.payload;
+      })
+      .addCase(fetchRedditPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message || null;
+      });
+  },
+});
+
+export default redditPostsSlice.reducer;
